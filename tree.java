b370d44376a64a3505b38846a59421068b3bcb7a
Prasenjit Chakraborty <cprasenj@thoughtworks.com>
2015-02-16 22:02:05 +0530

    tree insertion done with tests

--- tree.java
+++ tree.java
@@ -8,4 +8,39 @@ public class tree{
 		this.root = node;
 	}
 
-}
+	public int insert(int val) {
+		treeNode node = null;
+		if(root == null){
+			node = new treeNode(val); 
+			this.root = node;
+			return 1; 
+		}
+		if(root!=null){
+			if(root.data < val && root.rightChild == null){
+				node = new treeNode(val);
+				root.rightChild = node;
+				return 1; 
+			}
+			if(root.data > val && root.leftChild == null){
+				node = new treeNode(val);
+				root.leftChild = node;
+				return 1; 
+			}
+			if(root.data > val && root.leftChild != null){
+				tree subTree = new tree(this.root.leftChild);
+				subTree.insert(val);
+				return 1;
+			}
+			if(root.data < val && root.rightChild != null) {
+				tree subTree = new tree(this.root.rightChild);
+				subTree.insert(val);
+				return 1;
+			}
+		}
+		return 1;
+	}
+}	
+
+
+
+
--- treeTest.java
+++ treeTest.java
@@ -15,4 +15,51 @@ public class treeTest {
 		assertEquals(node.data,5);
 		assertEquals(tree.root.data,5);
 	}
+
+	@Test
+	public void insert_001(){
+		console.log("insert inserts value in the empty tree");
+		tree tree = new tree();
+		tree.insert(5);
+		assertEquals(tree.root.data,5);
+	}
+
+	@Test
+	public void insert_002(){
+		console.log("insert inserts value in the existing tree");
+		tree tree = new tree();
+		tree.insert(5);
+		tree.insert(10);
+		assertEquals(tree.root.rightChild.data,10);
+	}
+
+	@Test
+	public void insert_003(){
+		console.log("insert inserts value in the existing tree of depth 2");
+		tree tree = new tree();
+		tree.insert(5);
+		tree.insert(10);
+		tree.insert(12);
+		assertEquals(tree.root.rightChild.rightChild.data,12);
+	}
+
+	@Test
+	public void insert_004(){
+		console.log("insert inserts value in the existing tree");
+		tree tree = new tree();
+		tree.insert(5);
+		tree.insert(3);
+		assertEquals(tree.root.leftChild.data,3);
+	}
+
+	@Test
+	public void insert_005(){
+		console.log("insert inserts value in the existing tree of depth 2");
+		tree tree = new tree();
+		tree.insert(5);
+		tree.insert(3);
+		tree.insert(1);
+		assertEquals(tree.root.leftChild.leftChild.data,1);
+	}
+
 }
