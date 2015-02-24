351ab6db8bd73fd41cf03071addc2da924bd5e56
Prasenjit Chakraborty <cprasenj@thoughtworks.com>
2015-02-24 09:27:20 +0530

    renamed treeNodeTest.java file

--- treeNodeTest.java
+++ treeNodeTest.java
@@ -1,7 +1,7 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class treeNodeTest {
+public class TreeNodeTest {
 	
 	@Test
 	public void initially_treeNode_has_value_0_and_all_points_to_null_if_no_argument_passed() {
